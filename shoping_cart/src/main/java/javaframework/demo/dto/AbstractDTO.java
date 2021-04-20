ab1dc4b9683794ba291fb8015d26834aef75320d
DoanVan <vanbkat14121102@gmail.com>
2021-04-21 02:06:19 +0700

    vấn commit 1

---
+++ shoping_cart/src/main/java/javaframework/demo/dto/AbstractDTO.java
@@ -0,0 +1,28 @@
+package javaframework.demo.dto.paging;
+
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class AbstractDTO<T> extends PageResult<T> {
+    private Long id;
+    private boolean status;
+    private String createdBy;
+    private Date createdDate;
+    private String modifiedBy;
+    private Date modifiedDate;
+
+    // alert
+    private String alert;
+    private String message;
+
+    // Delete
+    private Long[] ids;
+
+    // Keep value search when searching
+    private String search;
+    // using to mark menu
+    private String active;
+}
